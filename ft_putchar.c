b21ad6b1d07be84618f3d39ccdddf76314483008
Stanley Horwood <stanley@horwood.fr>
2018-07-11 18:23:46 +0200

    ft_putchar added necessary library for the 'write' function

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,10 +6,12 @@
 /*   By: shorwood <shorwood@student.101.fr>         +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/06 14:53:46 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/11 17:56:59 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/11 18:23:19 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
