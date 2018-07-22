40289c291c6e6ba501720271c34dbd9d25d38ef8
Stanley Horwood <stanley@horwood.fr>
2018-07-22 17:09:19 +0200

    normed using ternaries

--- ft_strsqr.c
+++ ft_strsqr.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/22 15:29:51 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/22 16:14:07 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/22 17:04:59 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,21 +17,25 @@ char	*ft_strsqr(char *sym, unsigned int w, unsigned int h)
 {
 	char			*str;
 	char			*buf;
-	unsigned int 	i;
-	unsigned int 	j;
+	unsigned int	i;
+	unsigned int	j;
 
 	if (!(str = (char*)malloc((w * h + h + 1) * sizeof(char))))
 		return (str);
 	buf = str;
 	i = 0U;
-	while (i++ < h)
+	while (i < h)
 	{
 		j = 0U;
-		*buf++ = *sym++;
-		while (j++ <= w)
-			*buf++ = *sym;
-		sym++;
-		*buf++ = *sym++;
+		while (j < w)
+		{
+			*buf = *(sym + 1);
+			*buf = (j == w - 1) ? *(sym + 2) : *buf;
+			*buf++ = (j == 0) ? *sym : *buf;
+			j++;
+		}
+		sym += (i == 0) ? 3 : 0;
+		sym += (i++ == h - 2) ? 3 : 0;
 		*buf++ = '\n';
 	}
 	*buf = '\0';
