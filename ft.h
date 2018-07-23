6b71e71a3830e6ce7a178076bb1958939799e689
Minh an Tran <mintran@z3r3p4.le-101.fr>
2018-07-23 21:38:01 +0200

    proto

--- ft.h
+++ ft.h
@@ -6,7 +6,7 @@
 /*   By: mintran <mintran@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/21 19:23:29 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/23 20:47:52 by mintran     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/23 21:37:37 by mintran     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -44,8 +44,6 @@ t_list				*ft_list_new(void *data);
 void				ft_list_push_back(t_list **lst, void *data);
 int					ft_list_size(t_list *lst);
 int					ft_power(int x, unsigned int n);
-
-
 char				*ft_strread(int fld, int bfs);
 int					ft_strcmp(char *str, char *cmp);
 void				ft_putchar(char c);
@@ -54,14 +52,26 @@ void				ft_putnbr(int x);
 int					ft_strlen(char *str);
 char				*ft_strcat(char *dst, char *src);
 char				*ft_strchar(char *str, char *c);
-int					ft_strcmp(char *str, char *cmp);
 char				*ft_strcpy(char *dst, char *src);
 char				*ft_strdup(char *str);
 int					ft_strlen(char *str);
 int					ft_strlinecnt(char *str);
 int					ft_strlinelen(char *str);
+char				*ft_strlowcase(char *str);
+char				*ft_strncat(char *dst, char *src, int n);
+char				*ft_strncpy(char *dst, char *src, unsigned int n);
+char				*ft_strread(int fld, int bfs);
+char				*ft_strrect(char *s, unsigned int w, unsigned int h);
+char				*ft_strrev(char *str);
+void				strrpl(char *str, char c, char r);
+char				*ft_strstr(char *str, char *fnd);
+void				ft_strtrl(char *str, char *c, char *r);
+int					ft_strcmp(char *str, char *cmp);
+int					ft_strncmp(char *str, char *cmp, unsigned int n);
 char				*ft_s_strcat(char *dst, char *src);
 char				*ft_s_strncat(char *dst, char *src, unsigned int n);
 char				*ft_strrect(char *s, unsigned int w, unsigned int h);
+char				*ft_strlowcase(char *str);
+char				*ft_strupcase(char *str);
 
 #endif
