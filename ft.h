dbd2129dc7e9127faf6afcd6e6a230d997eb0a13
Minh an Tran <mintran@z3r3p4.le-101.fr>
2018-07-23 20:47:58 +0200

    proto

--- ft.h
+++ ft.h
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft.h                                             .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*   By: mintran <mintran@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/21 19:23:29 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/22 21:36:18 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/23 20:47:52 by mintran     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -34,10 +34,29 @@ typedef struct		s_3d
 }					t_3d;
 
 int					ft_atoi(char *str);
+int					ft_atoi_base(char *str, int base);
+int					ft_ctoi(char c);
+int					ft_factorial(int n);
+int					ft_fibonacci(int n);
+void				ft_list_clear(t_list **clr);
+t_list				*ft_list_last(t_list *lst);
+t_list				*ft_list_new(void *data);
+void				ft_list_push_back(t_list **lst, void *data);
+int					ft_list_size(t_list *lst);
+int					ft_power(int x, unsigned int n);
+
+
 char				*ft_strread(int fld, int bfs);
 int					ft_strcmp(char *str, char *cmp);
+void				ft_putchar(char c);
 void				ft_putstr(char *str);
 void				ft_putnbr(int x);
+int					ft_strlen(char *str);
+char				*ft_strcat(char *dst, char *src);
+char				*ft_strchar(char *str, char *c);
+int					ft_strcmp(char *str, char *cmp);
+char				*ft_strcpy(char *dst, char *src);
+char				*ft_strdup(char *str);
 int					ft_strlen(char *str);
 int					ft_strlinecnt(char *str);
 int					ft_strlinelen(char *str);
