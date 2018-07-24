6c9d44a9f12eb81579f2770420a9161ba662157f
Stanley Horwood <stanley@horwood.fr>
2018-07-24 03:22:12 +0200

    removed strchar + added strc

--- ft_strc.c
+++ ft_strc.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_strchar.c                                     .::    .:/ .      .::   */
+/*   ft_strc.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/21 02:33:12 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/22 17:19:14 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/24 03:21:54 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strchar(char *str, char *c)
+char	*ft_strc(char *str, char c)
 {
-	if (*str == *c && *c)
-		return (str);
-	if (*str != *c)
-		return (strchar(str + 1, c)
-			? strchar(str + 1, c)
-			: strchar(str, c + 1));
-		return (NULL);
+	while (*str)
+		if (*str++ == c)
+			return (str);
+	return (NULL);
 }
