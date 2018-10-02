a8384f3f5fbd14e35db02a46d518dcb54fa1dce0
Stanley Horwood <stanley@horwood.fr>
2018-10-02 15:43:31 +0200

    restructured project

--- Tests/ft_strlen_test.c
+++ Tests/ft_strlen_test.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft.c                                             .::    .:/ .      .::   */
+/*   ft_strlen_test.c                                 .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/07/21 02:49:42 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/22 18:16:09 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/10/02 15:40:11 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/02 15:41:39 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "ft_s_strcat.c"
-#include "ft_s_strncat.c"
+#include "../Includes/ft.h"
 
 int		main(void)
 {
-	char *str;
 
-	str = malloc(5 * sizeof(char));
-	*str = '\0';
-
-	printf("%s\n", ft_s_strncat(NULL, "dgtgr\0dawdaw", 23));
-	return (0);
 }
