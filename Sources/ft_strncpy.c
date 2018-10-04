92a56f4d5c8e764a7f80ad9731c0062f89f07dad
Stanley Horwood <stanley@horwood.fr>
2018-10-04 16:12:30 +0200

    correct prototype

--- Sources/ft_strncpy.c
+++ Sources/ft_strncpy.c
@@ -6,14 +6,14 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/22 05:26:02 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/04 16:07:00 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strncpy(char *dst, char *src, unsigned int n)
+char	*ft_strncpy(char *dst, const char *src, unsigned long n)
 {
 	char *buf;
 
