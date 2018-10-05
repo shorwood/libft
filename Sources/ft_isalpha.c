11e1a0567499761920ecd7b8a7455080fa657a42
Stanley Horwood <stanley@horwood.fr>
2018-10-05 16:53:29 +0200

    corrected typo in function name

--- Sources/ft_isalpha.c
+++ Sources/ft_isalpha.c
@@ -6,12 +6,12 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/10 05:58:53 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/02 20:30:24 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/05 16:40:12 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int		ft_isaplha(int c)
+int		ft_isalpha(int c)
 {
 	if ((c < 'a' || c > 'z')
 		&& (c < 'A' || c > 'Z'))
