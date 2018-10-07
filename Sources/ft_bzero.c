8edf56772a8d0db05182ad57796f30f97c2fd796
Stanley Horwood <stanley@horwood.fr>
2018-10-07 15:17:29 +0200

    changed increment timing

--- Sources/ft_bzero.c
+++ Sources/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 12:19:14 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 13:34:33 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ void	*ft_bzero(void *mem, size_t len)
 	uint8_t *buf;
 
 	buf = (uint8_t*)mem;
-	while (--len)
+	while (len--)
 		*buf++ = 0x00;
 	return (mem);
 }
