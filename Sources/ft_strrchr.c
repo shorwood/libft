89159bba8ceae1f995234bf450286174cd3ab7ca
Stanley Horwood <stanley@horwood.fr>
2018-10-07 11:16:38 +0200

    added strchr + strrchr and their tests

---
+++ Sources/ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strrchr.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/07 11:16:10 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+
+char	*ft_strrchr(const char *str, int c)
+{
+	char *buf;
+
+	buf = (char*)str;
+	while (*buf++)
+		;
+	while (buf != str)
+		if (*buf == c)
+			return ((char*)buf);
+		else
+			buf--;
+	return (NULL);
+}
---
+++ Tests/ft_strchr_test.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strchr_test.c                                 .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/02 22:16:39 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/07 11:11:11 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../Sources/ft_strchr.c"
+
+void	test(char *msg, const char *str, int c)
+{	
+	char *ret_libc;
+	char *ret_libft;
+
+	ret_libc = strchr(str, c);
+	ret_libft = ft_strchr(str, c);
+	printf
+	(
+		"| %-50s | %d | %-12p | %-12p |\n", 
+		msg,
+		ret_libc == ret_libft,
+		ret_libc,
+		ret_libft
+	);
+}
+
+int		main(void)
+{
+	char *spn;
+	spn = (char*)malloc(89 * sizeof(char*));
+
+	memset(spn, '-', 88);
+	printf("%s\n", spn);
+	test("Matching character at the start", "lorem lorem", 'l');
+	test("Matching character at the middle", "lorem lorem", 'r');
+	test("Matching character at the end", "lorem lorem", 'm');
+	test("Unatching character", "lorem lorem", 'q');
+	test("Empty string", "", 'a');
+	printf("%s\n", spn);
+	return (0);
+}
