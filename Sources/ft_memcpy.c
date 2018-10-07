b3cf0f82b666f3c3f0c71ac0879ac8123f704852
Stanley Horwood <stanley@horwood.fr>
2018-10-07 13:23:45 +0200

    fixed memcpy prototype and increment timing

--- Sources/ft_memcpy.c
+++ Sources/ft_memcpy.c
@@ -6,20 +6,19 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 12:17:41 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 13:14:28 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_memcpy(void *dst, const void *src)
+char	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	char *buf;
 
 	buf = dst;
-	while (*(uint8_t*)src)
+	while (len--)
 		*(uint8_t*)buf++ = *(uint8_t*)src++;
-	*buf = '\0';
 	return (dst);
 }
