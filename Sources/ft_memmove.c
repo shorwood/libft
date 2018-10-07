4e73f2c5a7f46388af450b6a68ccfb73e2bfde02
Stanley Horwood <stanley@horwood.fr>
2018-10-07 15:17:19 +0200

    changed condition order

--- Sources/ft_memmove.c
+++ Sources/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 13:18:13 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 15:15:19 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,11 +18,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char *buf;
 
 	buf = dst;
-	if (dst > src)
-		while (len--)
-			*(uint8_t*)(buf + len) = *(uint8_t*)(src + len);
-	else
+	if (dst < src)
 		while (len--)
 			*(uint8_t*)buf++ = *(uint8_t*)src++;
+	else
+		while (len--)
+			*(uint8_t*)(buf + len) = *(uint8_t*)(src + len);
 	return (dst);
 }
