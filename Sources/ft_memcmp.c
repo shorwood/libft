c3c213763f9d8bb95f0a600825ec41878bb7e0ca
Stanley Horwood <stanley@horwood.fr>
2018-10-07 12:09:13 +0200

    renamed variables

--- Sources/ft_memcmp.c
+++ Sources/ft_memcmp.c
@@ -6,18 +6,18 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 12:07:36 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 12:08:52 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		ft_memcmp(const void *blk, const void *cmp, size_t len)
+int		ft_memcmp(const void *mem, const void *cmp, size_t len)
 {
 	if (!len)
 		return (0);
-	if (*(unsigned char*)blk == *(unsigned char*)cmp)
-		return (ft_memcmp(++blk, ++cmp, --len));
-	return (*(unsigned char*)blk - *(unsigned char*)cmp);
+	if (*(unsigned char*)mem == *(unsigned char*)cmp)
+		return (ft_memcmp(++mem, ++cmp, --len));
+	return (*(unsigned char*)mem - *(unsigned char*)cmp);
 }
