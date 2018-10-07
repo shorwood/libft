2f7f9e267bbb836ecc77c0ceb14e3feb93a5a141
Stanley Horwood <stanley@horwood.fr>
2018-10-07 15:17:37 +0200

    changed increment timing

--- Sources/ft_memset.c
+++ Sources/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 12:19:30 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 14:47:24 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ void	*ft_memset(void *mem, int c, size_t len)
 	uint8_t *buf;
 
 	buf = (uint8_t*)mem;
-	while (--len)
+	while (len--)
 		*buf++ = c;
 	return (mem);
 }
