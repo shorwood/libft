8517e73c5d7896fcddb195ba757e573fa12b2d6e
Stanley Horwood <stanley@horwood.fr>
2018-10-07 11:25:32 +0200

    string functions header up to date

--- Includes/ft_string.h
+++ Includes/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/21 19:23:29 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/05 19:54:37 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 11:24:26 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,6 +16,18 @@
 #ifndef _FT_STRING_H
 # define _FT_STRING_H
 
+char	*ft_strcat(char *dst, const char *src);
+char	*ft_strchr(const char *str, int c);
+int		ft_strcmp(const char *str, const char *cmp);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strdup(const char *str);
+size_t	ft_strlcat(char *dst, const char *src, size_t len);
 size_t	ft_strlen(const char *str);
+char	*ft_strncat(char *dst, const char *src, size_t n);
+int		ft_strncmp(char *str, const char *cmp, size_t len);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strnstr(const char *big, const char *ltl, size_t len);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strchr(const char *str, int c);
 
 #endif
