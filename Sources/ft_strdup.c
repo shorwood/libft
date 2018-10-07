ddf5dc63624fa573fa7d1894ced9052a5187aa48
Stanley Horwood <stanley@horwood.fr>
2018-10-07 11:29:25 +0200

    malloc explicitly casted

--- Sources/ft_strdup.c
+++ Sources/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/05 15:12:37 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 11:22:59 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 11:29:02 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,7 +21,7 @@ char	*ft_strdup(const char *str)
 	buf = (char*)str;
 	while (*buf++)
 		;
-	if (!(dup = malloc((buf - str) * sizeof(char))))
+	if (!(dup = (char*)malloc((buf - str) * sizeof(char))))
 		return (NULL);
 	buf = dup;
 	while (*str)
