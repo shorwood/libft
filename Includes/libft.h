a2002ea5a12ee41ab91bebd29be4e8aea6f0eeb5
Stanley Horwood <stanley@horwood.fr>
2018-10-07 11:29:15 +0200

    renamed main header

--- Includes/libft.h
+++ Includes/libft.h
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft.h                                             .::    .:/ .      .::   */
+/*   libft.h                                          .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/21 19:23:29 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 09:23:26 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/07 11:27:56 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-#ifndef _FT_H
-# define _FT_H
+#ifndef _LIBFT_H
+# define _LIBFT_H
 
 #include "ft_ctype.h"
 #include "ft_macro.h"
