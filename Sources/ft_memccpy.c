c2d85882190a9281cd0ca57ec7a7c85846bb981e
Stanley Horwood <stanley@horwood.fr>
2018-10-09 16:28:34 +0200

    readability

--- Sources/ft_memccpy.c
+++ Sources/ft_memccpy.c
@@ -6,22 +6,20 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 13:14:36 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/09 09:59:01 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_memccpy(void *dst, const void *src, int c, size_t len)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t len)
 {
-	char *buf;
-
-	buf = dst;
 	while (len--)
-		if (*(uint8_t*)buf != (uint8_t)c)
-			*(uint8_t*)buf++ = *(uint8_t*)src++;
-		else
-			return (++buf);
+	{
+		*(uint8_t*)dst++ = *(uint8_t*)src;
+		if (*(uint8_t*)src++ == (uint8_t)c)
+			return (dst);
+	}
 	return (NULL);
 }
