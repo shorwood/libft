2d361a17aa6e03b712ab04b9cade0ababab2c229
Stanley Horwood <stanley@horwood.fr>
2018-10-09 16:29:13 +0200

    correct return type

--- Sources/ft_memcpy.c
+++ Sources/ft_memcpy.c
@@ -6,14 +6,14 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 13:14:28 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/09 09:16:48 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_memcpy(void *dst, const void *src, size_t len)
+void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	char *buf;
 
