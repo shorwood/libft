175051fa261a9cbc4082e1fa83a00d4ee21078c0
Stanley Horwood <stanley@horwood.fr>
2018-10-12 08:20:41 +0200

    strncmp now stops at end of string

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/09 16:23:31 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/12 08:19:13 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,7 +15,7 @@
 
 int		ft_strncmp(const char *str, const char *cmp, size_t len)
 {
-	while (len--)
+	while ((*str || *cmp) && len--)
 	{
 		if (*str != *cmp)
 			return ((unsigned char)*str - (unsigned char)*cmp);
