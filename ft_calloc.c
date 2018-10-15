4e511b41773bd09ea619ac4c99d6228525fafe29
Stanley Horwood <stanley@horwood.fr>
2018-10-15 14:08:00 +0200

    added allocation functions

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_calloc.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/15 13:12:52 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/15 13:54:45 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_calloc(size_t cnt, size_t siz)
+{
+	void 	*ptr;
+	uint8_t	*buf;
+
+	siz *= cnt;
+	if (!(ptr = malloc(siz)))
+		return (ptr);
+	buf = (uint8_t*)ptr;
+	while (siz--)
+		*buf++ = 0x00;
+	return (ptr);
+}
---
+++ ft_realloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_realloc.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/15 13:12:52 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/15 14:03:52 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_realloc(void *ptr, size_t siz)
+{
+	uint8_t	*old;
+	uint8_t	*buf;
+	void	*new;
+
+	if (!(new = malloc(siz)))
+		return (new);
+	old = (uint8_t*)ptr;
+	buf = (uint8_t*)new;
+	while(siz--)
+		*buf = *old;
+	free(ptr);
+	return (new);
+}
