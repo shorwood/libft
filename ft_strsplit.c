53101909f6c13e1bc9390a732f12060ed7df2a75
Stanley Horwood <stanley@horwood.fr>
2018-10-15 17:40:16 +0200

    corrected function behavior

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 13:12:52 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 17:16:15 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 17:35:34 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -34,37 +34,35 @@ static char	*helper_store(const char *dup, char c)
 
 static char	*helper_next(char *str, char c)
 {
-	while (*str && *str++ != c)
-		;
-	while (*str && *str++ == c)
-		;
-	return (*str ? (str - 1) : NULL);
+	while (*str && *str != c)
+		str++;
+	while (*str && *str == c)
+		str++;
+	return (*str ? str : NULL);
 }
 
 char		**ft_strsplit(char const *str, char c)
 {
 	char	*buf;
 	char	**arr;
-	char	**cur;
+	char	**wrd;
 	size_t	len;
 
 	while (*str && *str == c)
 		str++;
-	if (!*str)
-		return (NULL);
-	len = 2UL;
+	len = *str ? 2UL : 1UL;
 	buf = (char*)str;
 	while (buf && (buf = helper_next(buf, c)))
 		len++;
 	if (!(arr = (char**)malloc(len * sizeof(char*))))
 		return (arr);
-	cur = arr;
+	wrd = arr;
 	buf = (char*)str;
 	while (--len)
 	{
-		*cur++= helper_store(buf, c);
+		*wrd++ = helper_store(buf, c);
 		buf = helper_next(buf, c);
-	};
-	*cur = NULL;
+	}
+	*wrd = NULL;
 	return (arr);
 }
