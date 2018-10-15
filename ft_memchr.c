f65ea1e3292517f53ae5d9cb1c3769d7492403dc
Stanley Horwood <stanley@horwood.fr>
2018-10-15 23:04:57 +0200

    renamed variable

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,20 +6,20 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/09 16:28:46 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 22:52:59 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*ft_memchr(const void *mem, int byt, size_t len)
+void	*ft_memchr(const void *ptr, int byt, size_t len)
 {
 	while (len--)
 	{
-		if (*(uint8_t*)mem == (uint8_t)byt)
-			return (void*)(mem);
-		mem++;
+		if (*(uint8_t*)ptr == (uint8_t)byt)
+			return (void*)(ptr);
+		ptr++;
 	}
 	return (NULL);
 }
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,18 +6,18 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 12:19:02 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 22:53:49 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		ft_memcmp(const void *mem, const void *cmp, size_t len)
+int		ft_memcmp(const void *ptr, const void *cmp, size_t len)
 {
 	if (!len)
 		return (0);
-	if (*(uint8_t*)mem == *(uint8_t*)cmp)
-		return (ft_memcmp(++mem, ++cmp, --len));
-	return (*(uint8_t*)mem - *(uint8_t*)cmp);
+	if (*(uint8_t*)ptr == *(uint8_t*)cmp)
+		return (ft_memcmp(++ptr, ++cmp, --len));
+	return (*(uint8_t*)ptr - *(uint8_t*)cmp);
 }
