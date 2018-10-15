1d6b8ad5a27da167060acde074ff4b59070949ef
Stanley Horwood <stanley@horwood.fr>
2018-10-15 23:31:02 +0200

    return the malloc failure

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/05 15:12:37 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 11:29:02 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 22:54:38 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,7 +22,7 @@ char	*ft_strdup(const char *str)
 	while (*buf++)
 		;
 	if (!(dup = (char*)malloc((buf - str) * sizeof(char))))
-		return (NULL);
+		return (dup);
 	buf = dup;
 	while (*str)
 		*buf++ = *str++;
