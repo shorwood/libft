304e6187c01a1489fd83e6ea7614656590ef4895
Stanley Horwood <stanley@horwood.fr>
2018-10-15 23:03:13 +0200

    renamed variable

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,19 +6,19 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 13:34:33 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 22:51:13 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*ft_bzero(void *mem, size_t len)
+void	*ft_bzero(void *ptr, size_t len)
 {
 	uint8_t *buf;
 
-	buf = (uint8_t*)mem;
+	buf = (uint8_t*)ptr;
 	while (len--)
 		*buf++ = 0x00;
-	return (mem);
+	return (ptr);
 }
