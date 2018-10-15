2c961868c1312dadcea3978ab556c78994f9e1a8
Stanley Horwood <stanley@horwood.fr>
2018-10-15 23:05:10 +0200

    renamed variable

--- ft_memset.c
+++ ft_memset.c
@@ -6,19 +6,19 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/07 14:47:24 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 22:52:12 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*ft_memset(void *mem, int c, size_t len)
+void	*ft_memset(void *ptr, int c, size_t len)
 {
 	uint8_t *buf;
 
-	buf = (uint8_t*)mem;
+	buf = (uint8_t*)ptr;
 	while (len--)
 		*buf++ = c;
-	return (mem);
+	return (ptr);
 }
