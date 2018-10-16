d97b7bb4967634d54dbd3128e19a0bceea441cab
Stanley Horwood <stanley@horwood.fr>
2018-10-16 06:08:43 +0200

    added strndup for real now

---
+++ ft_strndup.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strndup.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/05 15:12:37 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/16 06:08:10 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strndup(const char *str, size_t len)
+{
+	char *buf;
+	char *dup;
+
+	if (!(dup = (char*)malloc(++len * sizeof(char))))
+		return (dup);
+	buf = dup;
+	while (*str && --len)
+		*buf++ = *str++;
+	*buf = '\0';
+	return (dup);
+}
