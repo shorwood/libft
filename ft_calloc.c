66af3c5d2d0a3d8c2196031cd7659f3db6316f2d
Stanley Horwood <stanley@horwood.fr>
2018-10-30 01:16:17 +0100

    used bzero function to set bytes to 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,20 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 13:12:52 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 17:25:24 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/30 01:15:49 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 void	*ft_calloc(size_t cnt, size_t siz)
 {
 	void	*ptr;
-	uint8_t	*buf;
 
 	siz *= cnt;
-	if (!(ptr = malloc(siz)))
-		return (ptr);
-	buf = (uint8_t*)ptr;
-	while (siz--)
-		*buf++ = 0x00;
+	if (!(ptr = ft_malloc(siz)))
+		return (NULL);
+	ft_bzero(ptr, siz);
 	return (ptr);
 }
