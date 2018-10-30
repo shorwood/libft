27c8d0cb55c613c2732920212476629e08a0db3d
Stanley Horwood <stanley@horwood.fr>
2018-10-30 02:50:31 +0100

    compacted reusing functions

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,25 +6,20 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 13:12:52 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 23:58:05 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/30 02:43:14 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 void	*ft_realloc(void *ptr, size_t siz)
 {
-	uint8_t	*old;
-	uint8_t	*buf;
 	void	*new;
 
-	if (!(new = malloc(siz)))
-		return (new);
-	old = (uint8_t*)ptr;
-	buf = (uint8_t*)new;
-	while (siz--)
-		*buf++ = *old++;
-	free(ptr);
+	if (!(new = ft_malloc(siz)))
+		return (NULL);
+	ft_memcpy(new, ptr, siz);
+	ft_free(ptr);
 	return (new);
 }
