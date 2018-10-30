632f6099ed6b675664a76efea023980ec903ade5
Stanley Horwood <stanley@horwood.fr>
2018-10-30 02:50:49 +0100

    explicit return

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/12 09:05:38 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 23:12:42 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/30 01:21:52 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,7 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 
 	if (!s1 || !s2)
-		return (char*)(0x00);
+		return (NULL);
 	len = 1UL;
 	buf = (char*)s1;
 	while (*buf++)
@@ -29,7 +29,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	while (*buf++)
 		len++;
 	if (!(str = (char*)malloc(++len * sizeof(char))))
-		return (str);
+		return (NULL);
 	buf = str;
 	while (*s1)
 		*buf++ = *(char*)s1++;
