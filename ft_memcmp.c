b4aa2c47f7f4e1acb207654a8d4bd46b910ffd4d
Stanley Horwood <stanley@horwood.fr>
2018-10-30 01:20:01 +0100

    rewritten function to be iterative

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,18 +6,21 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 22:08:58 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 22:53:49 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/17 18:41:15 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 int		ft_memcmp(const void *ptr, const void *cmp, size_t len)
 {
-	if (!len)
-		return (0);
-	if (*(uint8_t*)ptr == *(uint8_t*)cmp)
-		return (ft_memcmp(++ptr, ++cmp, --len));
-	return (*(uint8_t*)ptr - *(uint8_t*)cmp);
+	while (len--)
+	{
+		if (*(uint8_t*)ptr != *(uint8_t*)cmp)
+			return (int)(*(uint8_t*)ptr - *(uint8_t*)cmp);
+		ptr++;
+		cmp++;
+	}
+	return (0);
 }
