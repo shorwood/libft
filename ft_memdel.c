cbe33a338b866bbbab2223cf332e6cd1600d485e
Stanley Horwood <stanley@horwood.fr>
2018-10-30 02:46:20 +0100

    replaced free with interface function + changed header

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,17 +6,17 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/12 07:55:49 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 20:44:45 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/17 19:14:16 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 void	ft_memdel(void **ptr)
 {
 	if (!ptr || !*ptr)
 		return ;
-	free(*ptr);
+	ft_free(*ptr);
 	*ptr = NULL;
 }
