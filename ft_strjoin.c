1e48f96f98b937d5df786f7816b56e715440b588
Stanley Horwood <stanley@horwood.fr>
2018-11-08 10:09:00 +0100

    protected inputs and better errors handling

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,12 +6,12 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/12 09:05:38 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/30 01:21:52 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/07 11:36:07 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -19,8 +19,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*buf;
 	size_t	len;
 
-	if (!s1 || !s2)
-		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	len = 1UL;
 	buf = (char*)s1;
 	while (*buf++)
@@ -28,7 +30,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	buf = (char*)s2;
 	while (*buf++)
 		len++;
-	if (!(str = (char*)malloc(++len * sizeof(char))))
+	if (!(str = (char*)ft_malloc(++len * sizeof(char))))
 		return (NULL);
 	buf = str;
 	while (*s1)
