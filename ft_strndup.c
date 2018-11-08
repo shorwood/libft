eda447d948e0c44800210c16ffb8f7ea6d471292
Stanley Horwood <stanley@horwood.fr>
2018-11-08 19:48:39 +0100

    protected parameters

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/05 15:12:37 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/16 06:08:10 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/08 16:07:02 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,8 +18,10 @@ char	*ft_strndup(const char *str, size_t len)
 	char *buf;
 	char *dup;
 
+	if (!str)
+		return (NULL);
 	if (!(dup = (char*)malloc(++len * sizeof(char))))
-		return (dup);
+		return (NULL);
 	buf = dup;
 	while (*str && --len)
 		*buf++ = *str++;
--- ft_strnjoin.c
+++ ft_strnjoin.c
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/12 09:05:38 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/28 18:20:29 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/08 19:48:14 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,8 +18,10 @@ char	*ft_strnjoin(char const *s1, char const *s2, size_t len)
 	char	*str;
 	char	*buf;
 
-	if (!s1 || !s2)
-		return (NULL);
+	if (!s1)
+		return (ft_strndup(s2, len));
+	if (!s2)
+		return (ft_strdup(s1));
 	buf = (char*)s1;
 	while (*buf++)
 		len++;
