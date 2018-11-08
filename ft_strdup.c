c5798c53fa1d5c1243fea5f7eb929494878609e7
Stanley Horwood <stanley@horwood.fr>
2018-11-08 10:08:21 +0100

    protected strdup input

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,23 +6,25 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/05 15:12:37 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/15 22:54:38 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/08 10:07:55 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strdup(const char *str)
 {
 	char *buf;
 	char *dup;
 
+	if (!str)
+		return (NULL);
 	buf = (char*)str;
 	while (*buf++)
 		;
-	if (!(dup = (char*)malloc((buf - str) * sizeof(char))))
-		return (dup);
+	if (!(dup = (char*)ft_malloc((buf - str) * sizeof(char))))
+		return (NULL);
 	buf = dup;
 	while (*str)
 		*buf++ = *str++;
