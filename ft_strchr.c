78f04b1bb5b4ae03f71882b0dcdad61cb0e0e995
Stanley Horwood <stanley@horwood.fr>
2019-03-09 13:05:48 +0100

    optimized strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,20 +6,21 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/09 18:51:25 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/09 16:09:06 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/21 01:15:55 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *str, int chr)
 {
-	while (*str || !c)
-	{
-		if (*str == c)
-			return (char*)(str);
+	char	c;
+
+	if (!str)
+		return (NULL);
+	c = (char)chr;
+	while (*str && *str != c)
 		str++;
-	}
-	return (NULL);
+	return (*str == c ? (char*)str : NULL);
 }
