fd1c1376d55d83fd0b68c9374707020ace43b710
Stanley Horwood <stanley@horwood.fr>
2019-03-09 13:04:22 +0100

    new bit print for debug

--- ft_putnbits.c
+++ ft_putnbits.c
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_putbits.c                                     .::    .:/ .      .::   */
+/*   ft_putnbits.c                                    .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/16 22:03:32 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/21 03:08:41 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/14 17:54:43 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putbits(const void *ptr, size_t siz)
+void	ft_putnbits(const void *ptr, size_t siz, size_t n)
 {
     uint8_t *buf = (uint8_t*)ptr;
 	int		i;
 
-	while (siz--)
+	n++;
+	while (siz-- && n)
 	{
-		i = 8;
-		while (i)
-			ft_putchar((buf[siz] >> --i) & 1 ? '1' : '0');
+		i = __CHAR_BIT__;
+		while (i-- && --n)
+			ft_putchar((buf[siz] >> i) & 1 ? '1' : '0');
 	}
 }
