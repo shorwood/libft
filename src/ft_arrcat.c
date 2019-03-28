1623e35a3f00fa74ef3d5ab604f8cc0d3fcbc887
Stanley Horwood <stanley@horwood.fr>
2019-03-28 01:48:22 +0100

    added arrcat function

---
+++ src/ft_arrcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_arrcat.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/03/27 01:05:36 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2019/03/27 01:23:46 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_arrcat(const char **arr)
+{
+	size_t	len;
+	char	*str;
+	char	**buf;
+
+	buf = (char**)arr;
+	len = 0;
+	while (*buf)
+		len += ft_strlen(*buf++) + 1;
+	str = ft_strnew(len);
+	str[len] = '\0';
+
+	buf = (char**)arr;
+	len = 0;
+	while (*buf)
+	{
+		ft_strcpy(str + len, *buf++);
+		len += ft_strcspn(str + len, "\0") + 1;
+		str[len - 1] = '\n';
+	}
+
+
+	return (str);
+}
