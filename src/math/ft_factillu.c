951706ec08a3dacd313e2a3ddd38be27e0975253
Stanley Horwood <stanley@horwood.fr>
2019-04-06 14:02:02 +0200

    added factorial math function

---
+++ src/math/ft_factillu.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_factillu.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/03/26 22:29:58 by shorwood     #+#   ##    ##    #+#       */
+/*   Updated: 2019/04/05 10:46:13 by shorwood    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+unsigned long long	ft_factillu(unsigned long long n)
+{
+	unsigned long long ret;
+	unsigned long long i;
+
+	i = n;
+	ret = 1ULL;
+	while (i > 1ULL)
+		ret *= i--;
+	return (ret);
+}
