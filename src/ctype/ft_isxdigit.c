f5a9deab683bfe5abb70cc635eec6ba7709ec277
Stanley Horwood <stanley@horwood.fr>
2019-04-10 08:28:38 +0200

    added tab for normalization

--- src/ctype/ft_isxdigit.c
+++ src/ctype/ft_isxdigit.c
@@ -6,14 +6,14 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/10 05:58:53 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/30 01:39:00 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/10 04:58:22 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isxdigit(int c)
+int		ft_isxdigit(int c)
 {
 	return ((c >= 'a' && c <= 'f')
 		|| (c >= 'A' && c <= 'F')
